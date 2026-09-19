40b0430fb2c862462a9130da770ffe3501885c87
OceanHub <adrienfavela2030@gmail.com>
2026-09-19 14:02:45 -0700

    Initialize package.json for Discord bot project

---
+++ package.js
@@ -0,0 +1,12 @@
+{
+  "name": "vvll-bot",
+  "version": "1.0.0",
+  "description": "VVLL | Vx Vnilla Landon League Discord Bot",
+  "main": "index.js",
+  "scripts": {
+    "start": "node index.js"
+  },
+  "dependencies": {
+    "discord.js": "^14.25.1"
+  }
+}
